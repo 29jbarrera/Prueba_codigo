bc11f3879e52fc28a9b2de433608ff2b48854bb7
Javi Barrera <javierbarreralopez97@gmail.com>
2023-03-04 12:43:03 +0100

    Proyecto nube

---
+++ hola.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Primer documento php</title>
+  </head>
+  <body>
+    <?php
+        echo "Soy un artista";
+        ?>
+  </body>
+</html>
